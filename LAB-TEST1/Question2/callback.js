9ac8501939a58eb2755a02bb07ad9f5307a4e762
Hardik Kumar . <hardikgugnani123@gmail.com>
2024-10-03 17:06:35 -0400

    question 2

---
+++ LAB-TEST1/Question2/callback.js
@@ -0,0 +1,21 @@
+// Function to simulate a successful delayed response
+const delayedSuccess = () => {
+    setTimeout(() => {
+      let success = { message: 'delayed success!' }; 
+      console.log(success); 
+    }, 500); 
+  };
+  
+  const delayedException = () => {
+    setTimeout(() => {
+      try {
+        throw new Error('error: delayed exception!'); 
+      } catch (e) {
+        console.error(e); 
+      }
+    }, 500); 
+  };
+  
+  delayedSuccess();
+  delayedException();
+  
