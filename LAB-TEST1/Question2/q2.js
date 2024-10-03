e39a8a07e53fd305859d8837b1d89ae84f34ef6a
Hardik Kumar . <hardikgugnani123@gmail.com>
2024-10-03 18:40:28 -0400

    fixing q2

--- LAB-TEST1/Question2/q2.js
+++ LAB-TEST1/Question2/q2.js
@@ -1,24 +1,31 @@
-const resolvedPromise = () => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve({ message: 'delayed success' }); 
-      }, 500); 
-    });
-  };
-  
-  const rejectedPromise = () => {
-    return new Promise((_, reject) => {
-      setTimeout(() => {
-        reject({ error: 'delayed exception' }); 
-      }, 500); 
-    });
-  };
-  
-  resolvedPromise()
-    .then((result) => console.log(result)) 
-    .catch((error) => console.error(error)); 
-  
-  
-  rejectedPromise()
-    .then((result) => console.log(result))
-    .catch((error) => console.error(error));  
+function resolvedPromise() {
+  return new Promise(function(resolve) {
+      setTimeout(function() {
+          resolve({ message: 'delayed success' });
+      }, 500);
+  });
+}
+
+function rejectedPromise() {
+  return new Promise(function(resolve, reject) {
+      setTimeout(function() {
+          reject(new Error('delayed exception'));
+      }, 500);
+  });
+}
+
+resolvedPromise()
+  .then(function(result) {
+      console.log(result);
+  })
+  .catch(function(error) {
+      console.error(error);
+  });
+
+rejectedPromise()
+  .then(function(result) {
+      console.log(result);
+  })
+  .catch(function(error) {
+      console.error(error);
+  });
