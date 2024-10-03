195ecdd2e9cf1e0883d08ce9fa5e77bc4316098a
Hardik Kumar . <hardikgugnani123@gmail.com>
2024-10-03 18:34:29 -0400

    fixing q1

--- LAB-TEST1/Question1/q1.js
+++ LAB-TEST1/Question1/q1.js
@@ -1,21 +1,24 @@
 function lowerCaseWords(arr) {
-    
-    return new Promise((resolve, reject) => {
-      
+  return new Promise((resolve, reject) => {
       if (!Array.isArray(arr)) {
-        reject('The input should be an array!'); 
-      } else {
-        const lowercaseStrings = arr.filter(item => typeof item === 'string')
-                                    .map(word => word.toLowerCase()); 
-        
-        resolve(lowercaseStrings);
+          return reject(new Error('The input should be an array!'));
       }
-    });
-  }
-  
-  const mixedArray = ['PIZZA', 10, true, 25, false, 'wings'];
-  
-  lowerCaseWords(mixedArray)
-    .then(result => console.log(result))  
-    .catch(error => console.log(error)); 
-  
+
+      const lowercaseStrings = [];
+      
+      for (let i = 0; i < arr.length; i++) {
+          const item = arr[i];
+          if (typeof item === 'string') {
+              lowercaseStrings.push(item.toLowerCase());
+          }
+      }
+
+      resolve(lowercaseStrings);
+  });
+}
+
+const mixedArray = ['PIZZA', 10, true, 25, false, 'wings'];
+
+lowerCaseWords(mixedArray)
+  .then(result => console.log(result))  
+  .catch(error => console.log(error.message));
